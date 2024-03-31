b9cab23d002854f79e0a8f1f308125d537861702
Nguyen Truong Son <son.nt@cinno.work>
2024-03-31 22:28:36 +0700

    Sửa lỗi kết nối PB do để file nhầm chỗ

--- +layout.server.ts
+++
@@ -1,7 +0,0 @@
-import { serializeNonPOJOs } from '$lib/utils';
-import type { LayoutServerLoad } from './$types';
-
-export const load = (async ({ locals }) => {
-	const user = locals.pb.authStore.model;
-	return { user: serializeNonPOJOs(user) };
-}) satisfies LayoutServerLoad;
